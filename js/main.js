dc083e69cf33df1e75dbedc3f39e0c6f86ba0307
Kelvin Kiptum Kiprop <kelvin.kiprop96@gmail.com>
2021-12-08 14:29:46 +0300

    # Index - added introJs
    # General
    - added introJS vendor library

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function () {
+  // Intro.js scans the webpage and finds all elements with `data-hint` attribute
+  introJs().start();
+  introJs().addHints();
+
   setTimeout(() => {
     $('#loader-wrapper').addClass('d-none', {duration: 300});
     $('#content-wrapper').removeClass('d-none', {duration: 300});
